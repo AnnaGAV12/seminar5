9bd39c61581e81994348ba1d98c60a84c662594d
Anna <ds_anna@bk.ru>
2022-05-29 14:51:15 +0300

    Выяснить, являютя ли три числа сторонами треугольника

---
+++ seminar2305/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, являютя ли три числа сторонами треугольника
+Console.Write("Введите 1 сторону треугольника: ");
+int Side1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите 2 сторону треугольника: ");
+int Side2 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите 3 сторону треугольника: ");
+int Side3 = int.Parse(Console.ReadLine() ?? "0");
+if ((Side1 + Side2) > Side3 && (Side1 + Side3) > Side2 && (Side3 + Side2) > Side1)
+    Console.WriteLine("Является сторонами треугольника ");
+else 
+    Console.WriteLine("Не является сторонами треугольника ");
