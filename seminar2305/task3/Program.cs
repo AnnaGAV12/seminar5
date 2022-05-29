0bd5a97087997b55cff6dda5eec62a2932c47838
Anna <ds_anna@bk.ru>
2022-05-29 15:26:10 +0300

    Написать программу преобразования десятичного чсила в двоичное 1вариант

---
+++ seminar2305/task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу преобразования десятичного чсила в двоичное
+Console.WriteLine("Number? ");
+int number = int.Parse(Console.ReadLine());
+int tmpnumber = number;
+int tmp = 0;
+int count = 0;
+void FillArray(int[] array)
+{
+    int tmp = 0;
+    int index = 0;
+    int length = array.Length;
+    while (index < length)
+    {
+        tmp = number % 2;
+        number = number / 2;
+        array[index] = tmp;
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+while (tmpnumber > 0)
+{
+    tmp = tmpnumber % 2;
+    tmpnumber = tmpnumber / 2;
+    count++;
+}
+int[] array = new int[count];
+FillArray(array);
+PrintArray(array.Reverse().ToArray()); //Reverse array
+
