c0f4ffa8030f824bf71d3312b5122fb5daeb5ae1
Anna <ds_anna@bk.ru>
2022-06-04 18:05:33 +0300

    показывает первые N чисел Фибоначчи

---
+++ seminar2305/task4-2/Program.cs
@@ -0,0 +1,13 @@
+﻿// //Второй вариант: показывает первые N чисел Фибоначчи
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+int a = 1;
+int b = 0;
+int result;
+for (int i = 0; i < n; i++)
+{
+    result = a + b;
+    Console.Write(result + " ");
+    a = b;
+    b = result;
+}
