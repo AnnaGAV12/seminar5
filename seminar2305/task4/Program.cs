7f0e3f6e4ac08788655459914e7861f152b69282
Anna <ds_anna@bk.ru>
2022-06-04 18:05:07 +0300

    Показать числа Фибоначчи меньше заданного числа N

---
+++ seminar2305/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N
+// первый вариант
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+int firstValue = 0;
+int secondValue = 1;
+int result = 0;
+Console.Write($"{firstValue} {secondValue}");
+
+while (firstValue + secondValue < N)
+{
+    result = firstValue + secondValue;
+    Console.Write(result + " ");
+    firstValue = secondValue;
+    secondValue = result;
+}
+
+
